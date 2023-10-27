599f0ecf88a3f30527027adafe536f6f3a910442
Bruno Oliveira <bds.commus@gmail.com>
2023-10-26 21:55:02 -0300

    feat: add links to site

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const links = document.querySelectorAll('#link-inscricao');
+
+links.forEach((link) => {
+    link.setAttribute('href', 'https://www.sympla.com.br/lancamento-das-comunidades-de-php-do-estado-do-piaui__2207864');
+});
