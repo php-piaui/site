051337c7dac1c2a815e9ac117192c730ea5318d9
Bruno Oliveira <bds.commus@gmail.com>
2025-07-31 12:48:52 -0300

    Update CFP.vue

--- src/components/CFP.vue
+++ src/components/CFP.vue
@@ -138,7 +138,7 @@ const handleCFPClick = () => {
                     <svg class="w-4 h-4 text-violet-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    <span class="text-violet-100">Prazo: 31 de Julho</span>
+                    <span class="text-violet-100">Prazo: 11 de Agosto</span>
                   </div>
                   <div class="flex items-center gap-2">
                     <svg class="w-4 h-4 text-violet-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
